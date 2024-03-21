5f0bcd3de9640a0e8b51cdcc59d2a6737e86f722
Sachin <sachnaror@gmail.com>
2024-03-22 00:16:18 +0530

    Each day offers a lesson in perseverance

--- employee_app/admin.py
+++ employee_app/admin.py
@@ -1,3 +1,12 @@
+
 from django.contrib import admin
 
 # Register your models here.
+from . import *
+
+admin.site.register(models.Employees)
+admin.site.register(models.Salaries)
+admin.site.register(models.Departments)
+admin.site.register(models.DeptEmp)
+admin.site.register(models.DeptManager)
+admin.site.register(models.Titles)
